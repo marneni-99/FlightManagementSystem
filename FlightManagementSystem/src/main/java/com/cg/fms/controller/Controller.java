bbf13f522dd30228278106f16d20caf1d646178c
marneni <marnenisindhu2@gmail.com>
2020-09-24 19:51:18 +0530

    Fourth commit

--- FlightManagementSystem/src/main/java/com/cg/fms/controller/Controller.java
+++ FlightManagementSystem/src/main/java/com/cg/fms/controller/Controller.java
@@ -5,11 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cg.fms.dto.Booking;
+import com.cg.fms.exception.IdNotFoundException;
 import com.cg.fms.service.IService;
 
 @RestController
@@ -23,17 +24,15 @@ public class Controller {
 	
 	//For canceling flight ticket
 	@DeleteMapping("/CancelTickets/{id}")
-	public ResponseEntity<String> addBookingDetails(@PathVariable("id") long id){
-
+	public ResponseEntity<String> addBookingDetails(@PathVariable("id") long id) throws IdNotFoundException{
 		boolean b= flightScheduleService.cancelTickets(id);
 		if(!b)
 			return new ResponseEntity<String>("Id doesnt exist",HttpStatus.OK);
-		else		
-			return new ResponseEntity<String>("Cancelled tickets",HttpStatus.OK);
-
+		else	
+			throw new IdNotFoundException("Cancelled tickets");
 	}
-
-	
-	
-
+	@ExceptionHandler(IdNotFoundException.class)
+	public ResponseEntity<String> idNotFound(IdNotFoundException e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
+	}
 }
---
+++ FlightManagementSystem/src/main/java/com/cg/fms/exception/IdNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cg.fms.exception;
+
+public class IdNotFoundException extends Exception{
+	
+	public IdNotFoundException(String message)
+	{
+		super(message);
+	}
+
+}
